1b0058395a22da744b6525c9eb0d720007fdf7dc
whaohan <2778351280@qq.com>
2021-11-14 11:03:45 +0800

    fix: can not use static files

--- app/index.ts
+++ app/index.ts
@@ -11,10 +11,10 @@ const app = new Koa();
 app.use(cors()); // Allow CORS (Cross-Origin Resource Sharing)
 app.use(helmet()); // Provide security headers
 app.use(bodyParser({ multipart: true })); // Resolve request body
-app.use(staticServer('static')); // Access static resources
-app.use(router.routes()); // Set up the router
 app.use(views('views',{
 	extension:'ejs'
 }));
+app.use(staticServer('static')); // Access static resources
+app.use(router.routes()); // Set up the router
 
 export default app;
